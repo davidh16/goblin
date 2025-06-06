33a617273a77dddd1e6929b2eb894d49638965be
horvat <david@n-r.hr>
2025-06-06 12:52:39 +0200

    chore: stuff

--- commands/middleware/command.go
+++ commands/middleware/command.go
@@ -1,1 +1,19 @@
 package middleware
+
+import "github.com/spf13/cobra"
+
+var MiddlewareCmd = &cobra.Command{
+	Use:   "middleware",
+	Short: "Generate a middleware",
+	Run: func(cmd *cobra.Command, args []string) {
+		middlewareCmdHandler()
+	},
+}
+
+func middlewareCmdHandler() {
+	// list existing middlewares
+
+	// choose which ones to implement from ones that don't exist
+
+	// generate
+}
